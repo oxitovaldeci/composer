f18cd15d797c98106e5ef1be47265c60847d6694
oxitovaldeci <woshington.valdeci@ifsertao-pe.edu.br>
2021-10-01 00:08:05 -0300

    wsgi

--- composer/settings.py
+++ composer/settings.py
@@ -165,11 +165,9 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
-if not DEBUG: 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-print(DEBUG)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
