095750b34b70bf87998709a6ee4e20323b2b42a4
oxitovaldeci <woshington.valdeci@ifsertao-pe.edu.br>
2021-09-30 23:29:10 -0300

    statif files

--- composer/settings.py
+++ composer/settings.py
@@ -164,8 +164,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG: 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
