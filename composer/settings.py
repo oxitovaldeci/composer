9c1d821b89325fd10bd3fda1f941639509653979
oxitovaldeci <woshington.valdeci@ifsertao-pe.edu.br>
2021-11-08 21:57:52 -0300

    collectstatic s3

--- composer/settings.py
+++ composer/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'composer.urls'
@@ -171,3 +172,7 @@ if DEBUG:
     )
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
